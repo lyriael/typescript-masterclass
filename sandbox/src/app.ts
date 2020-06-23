01817b7ec2804eda63e99acae77e81c455f3fd4b
Judith Fuog <lyriael@gmx.net>
2020-06-23 17:44:26 +0200

    TS-MC: 21. Interfaces vs Classes

--- sandbox/src/app.ts
+++ sandbox/src/app.ts
@@ -1,21 +1,13 @@
-interface Item {
-    name: string;
+// interface Artist {
+//     name: string;
+// }
+
+class ArtistCreator /*implements Artist*/ {
+    constructor(public name: string) {}
 }
 
-interface Artist extends Item {
-    songs: number;
+function artistFactory({ name }: ArtistCreator) {
+    return new ArtistCreator(name);
 }
 
-interface Artist {
-    getSongs(): number;
-}
-
-type Artist2 = {name: string} & Item
-
-const newArtist: Artist = {
-    name: 'asdf',
-    songs: 7,
-    getSongs(): number {
-        return this.songs;
-    }
-};
+artistFactory({name: 'Foo'});
