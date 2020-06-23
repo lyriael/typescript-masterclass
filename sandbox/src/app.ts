41e9e2472b3f667fb0aef49ef437f031bc975c3e
Judith Fuog <lyriael@gmx.net>
2020-06-23 14:41:27 +0200

    TS-MC: 13. "Record" Mapped Type

--- sandbox/src/app.ts
+++ sandbox/src/app.ts
@@ -1,1 +1,13 @@
-console.log('Hello Typscript Masterclass!');
+let dictionary: Record<string, TrackStates> = {};
+
+interface TrackStates {
+    current: string;
+    next: string;
+}
+
+const item: Record<keyof TrackStates, string> = {
+    current: 'cd06uio',
+    next: 'cs98aos',
+};
+
+dictionary[0] = item;
