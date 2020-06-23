95914dede5c79f0c90093513e57307ddd0e72e06
Judith Fuog <lyriael@gmx.net>
2020-06-23 17:37:58 +0200

    TS-MC: 20. Interfaces vs Type Aliases

--- sandbox/src/app.ts
+++ sandbox/src/app.ts
@@ -1,49 +1,21 @@
-interface Order {
-    id: string;
-    amount: number;
-    currency: string;
+interface Item {
+    name: string;
 }
 
-interface Stripe {
-    type: 'stripe';
-    card: string;
-    cvc: string;
+interface Artist extends Item {
+    songs: number;
 }
 
-interface PayPal {
-    type: 'paypal';
-    email: string;
+interface Artist {
+    getSongs(): number;
 }
 
-type CheckoutCard = Order & Stripe;
-type CheckoutPayPal = Order & PayPal;
+type Artist2 = {name: string} & Item
 
-const order: Order = {
-    id: 'xj27s',
-    amount: 100,
-    currency: 'USD'
-};
-
-const orderCard: CheckoutCard = {
-    ...order,
-    type: 'stripe',
-    card: '1000 2000 3000 400',
-    cvc: '123',
-};
-
-const orderPayPal: CheckoutPayPal = {
-    ...order,
-    type: 'paypal',
-    email: 'over@abr.foo',
-};
-
-type Payload = CheckoutCard | CheckoutPayPal;
-
-function checkout(payload: Payload) {
-    if (payload.type === 'stripe') {
-        console.log(payload.card, payload.cvc);
+const newArtist: Artist = {
+    name: 'asdf',
+    songs: 7,
+    getSongs(): number {
+        return this.songs;
     }
-    if (payload.type === 'paypal') {
-        console.log(payload.email);
-    }
-}
+};
