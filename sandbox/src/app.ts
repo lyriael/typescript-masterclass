e52df3909a2f59dbc2a964370ac816d2f8b87f87
Judith Fuog <lyriael@gmx.net>
2020-06-23 16:57:23 +0200

    TS-MC: 16. User Defined Type Guards

--- sandbox/src/app.ts
+++ sandbox/src/app.ts
@@ -1,33 +1,40 @@
-function foo(bar: string | number) {
-    if (typeof bar === 'string') {
-        // string
-        return bar;
-    }
-    // number
-    return bar;
+class Foo {
+    bar() {}
 }
 
+function Foo2(){}
+Foo2.prototype.bar = function () {};
+
+const bar = new Foo();
+
+// console.log(bar instanceof Foo);
+// console.log(Object.getPrototypeOf(bar) === Foo.prototype);
 
 class Song {
-    constructor(public title: string, public duration: string | number) {}
-}
-
-function getSongDuration(item: Song) {
-    if (typeof item.duration === 'string') {
-        return item.duration;
+    constructor(public title: string, public duration: number) {
     }
-    const {duration} = item;
-    const minutes = Math.floor(duration / 60000);
-    const seconds = (duration / 1000) % 60;
-    return `${minutes}:${seconds}`
 }
 
-const songDurationFromString = getSongDuration(
-    new Song('Wonderful Wonderful', '05:31')
-);
-console.log(songDurationFromString);
+class Playlist {
+    constructor(public name: string, public songs: Song[]) {
+    }
+}
 
-const songDurationFromMS = getSongDuration(
-    new Song('Wonderful, Wonderful', 330000)
+function isSong(item: any): item is Song {
+    return item instanceof Song;
+}
+
+function getItemName(item: Song | Playlist) {
+    if (isSong(item)) {
+        return item.title;
+    }
+    return item.name;
+}
+
+const songName = getItemName(new Song('Wonderful, Wonderful', 300000));
+console.log('Song name: ', songName);
+
+const playlistName = getItemName(
+    new Playlist('The Best Songs', [new Song('The Man', 300000)])
 );
-console.log(songDurationFromMS);
+console.log('Song name: ', playlistName);
