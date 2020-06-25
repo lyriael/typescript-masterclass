95ea941d6eb23fe60c318f905e7dca310c3821bb
Judith Fuog <lyriael@gmx.net>
2020-06-25 10:20:18 +0200

    TS-MC: 28. Augmenting Modules with Declarations

--- sandbox/src/@types/lodash/index.d.ts
+++ sandbox/src/@types/lodash/index.d.ts
@@ -1,3 +1,7 @@
+import * as lodash from 'lodash';
+
 declare module 'lodash' {
-    export function chunk(collection: any, size?: number): any[][];
+    interface LoDashStatic {
+        log(item: string): void;
+    }
 }
